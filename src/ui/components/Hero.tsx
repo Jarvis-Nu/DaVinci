26a1bd9cdeea3e2321cf868056bf8af841480cda
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-26 18:13:27 +0100

    suprememods

---
+++ src/ui/components/Hero.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+export default function Hero() {
+    return(
+        <div className="w-full">
+            <div>
+                <Navbar />
+            </div>
+        </div>
+    )
+}
---
+++ src/ui/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Amadeus</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='w-full h-full'></main>
+    </div>
+  )
+}
+
+export default Home
