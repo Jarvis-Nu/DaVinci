c307879b4da8f828e3dc55d37c06a11de3a22c24
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-01-26 18:44:19 +0100

    suprememods

--- src/ui/components/Hero.tsx
+++ src/ui/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Navbar from "./Navbar";
 
 interface Props {
@@ -9,6 +10,21 @@ export default function Hero({ page }: Props) {
         <div className="w-full">
             <div>
                 <Navbar page={page} />
+            </div>
+            <div className="w-full px-[100px] h-full mt-36">
+                <div className="w-full flex justify-between items-center">
+                    <div className="w-full max-w-[700px] space-y-5">
+                        <h1 className="text-[60px] leading-[73px] font-semibold">A <span className="text-purple underline underline-offset-8">Decentralized</span>
+                         Audio Curation Platform and Database <span className="text-purple underline underline-offset-8">DAO</span>
+                        </h1>
+                        <h6 className="text-[24px] leading-[36px]">
+                            Welcome to the Music Data DAO, where we use blockchain technology to empower musicians and music industry professionals.
+                        </h6>
+                    </div>
+                    <div className="w-full max-w-[450px]">
+                        <Image src={"/music.png"} width={450} height={450} alt="" />
+                    </div>
+                </div>
             </div>
         </div>
     )
